ba6180d9012b5ee05a0759283b9c1c542bd34aa9
PLUXIN <pluxin485@gmail.com>
2025-05-30 22:08:56 -0400

    Creación de la carpeta static/css y js
    Creación de ambas carpetas junto a media

--- Django_Proyect/slide_away/slide_away/settings.py
+++ Django_Proyect/slide_away/slide_away/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = 'media/'
+MEDIA_URL = [
+    os.path.join(BASE_DIR, 'media'),
+]
